84002326f4359f6c9ebae108d6b56a807354809c
Michal Mazurek <akfaew@gmail.com>
2023-09-10 07:04:56 +0200

    update modules

--- ahocorasick_test.go
+++ ahocorasick_test.go
@@ -7,7 +7,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/akfaew/test"
+	"github.com/akfaew/utils/fixture"
+	"github.com/stretchr/testify/require"
 )
 
 func Read(t *testing.T, filename string) ([][]byte, error) {
@@ -16,7 +17,7 @@ func Read(t *testing.T, filename string) ([][]byte, error) {
 	dict := [][]byte{}
 
 	f, err := os.OpenFile("testdata/input/"+filename, os.O_RDONLY, 0660)
-	test.NoError(t, err)
+	require.NoError(t, err)
 
 	r := bufio.NewReader(f)
 	for {
@@ -33,44 +34,44 @@ func Read(t *testing.T, filename string) ([][]byte, error) {
 
 func TestBuild(t *testing.T) {
 	keywords, err := Read(t, "keywords")
-	test.NoError(t, err)
+	require.NoError(t, err)
 
 	m := new(Machine)
-	test.NoError(t, m.Build(keywords))
+	require.NoError(t, m.Build(keywords))
 }
 
 func TestMultiPatternSearch(t *testing.T) {
 	keywords, err := Read(t, "keywords")
-	test.NoError(t, err)
+	require.NoError(t, err)
 	m := new(Machine)
-	test.NoError(t, m.Build(keywords))
+	require.NoError(t, m.Build(keywords))
 
 	content := []byte("ushers she she")
 	terms := m.MultiPatternSearch(content, false)
-	test.Fixture(t, terms)
+	fixture.Fixture(t, terms)
 }
 
 func TestMultiPatternSearchQuick(t *testing.T) {
 	keywords, err := Read(t, "keywords")
-	test.NoError(t, err)
+	require.NoError(t, err)
 	m := new(Machine)
-	test.NoError(t, m.Build(keywords))
+	require.NoError(t, m.Build(keywords))
 
 	content := []byte("ushers she she")
 	ret := m.MultiPatternSearchQuick(content)
-	test.Fixture(t, ret)
+	fixture.Fixture(t, ret)
 }
 
 func TestExactSearch(t *testing.T) {
 	keywords, err := Read(t, "keywords")
-	test.NoError(t, err)
+	require.NoError(t, err)
 	m := new(Machine)
-	test.NoError(t, m.Build(keywords))
+	require.NoError(t, m.Build(keywords))
 
 	for _, k := range keywords {
 		if m.ExactSearch(k) == nil {
 			t.Error("exact search failed")
 		}
 	}
-	test.Len(t, keywords, 4)
+	require.Len(t, keywords, 4)
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.12
 
 require (
 	github.com/akfaew/darts v0.0.0-20190713084632-c9f684fef186
-	github.com/akfaew/test v0.0.0-20190601085208-e787bce5d914
+	github.com/akfaew/utils v0.0.0-20230910045320-6b3e12f00892
 	github.com/anknown/ahocorasick v0.0.0-20170415101647-0c5fc0283558
-	github.com/anknown/darts v0.0.0-20151216065714-83ff685239e6
+	github.com/anknown/darts v0.0.0-20151216065714-83ff685239e6 // indirect
 	github.com/cloudflare/ahocorasick v0.0.0-20131126104932-1ce46e42b741
+	github.com/stretchr/testify v1.8.4
 )
